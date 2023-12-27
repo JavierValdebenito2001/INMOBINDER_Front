753f6aa3d9fe1c753adecdc870758f74d4a974d6
Luis Guzman <felipeguzman20@gmail.com>
2023-12-27 14:44:55 -0300

    Add .env file and configure react-native-dotenv plugin

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,14 +1,80 @@
 import { useNavigation } from '@react-navigation/native';
+import * as Location from 'expo-location';
+import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+
+  const casaVerde = require('../../../assets/images/INMOBINDER-06.png');
+
+
 
 export function HomeScreen() {
   const navigation = useNavigation();
 
+  const [origin, setOrigin] = React.useState({
+
+    latitude: -33.451796,
+    longitude: -70.667609,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
+  });
+
+  React.useEffect(() => {
+    getLocationPermission();
+  }, []);
+  
+  async function getLocationPermission() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    // Utiliza la variable "status" aquí
+    if (status !== 'granted') {
+      console.log('Permission to access location was denied');
+
+      return;
+
+    } else {
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setOrigin({
+        ...origin,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    }
+  }
+
+ 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView 
+      style={styles.map} 
+      initialRegion={{
+        latitude: origin.latitude,
+        longitude: origin.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+      > 
+        <Marker 
+          draggable={true}
+           coordinate={origin}
+          onDragEnd={(direction) => setOrigin({
+          ...origin,
+          latitude: direction.nativeEvent.coordinate.latitude,
+          longitude: direction.nativeEvent.coordinate.longitude,
+          })}
+          title="Mi ubicación"
+          description="Mi ubicación actual"
+          >
+          <Image
+            source={casaVerde}
+            style={{ width: 30, height: 30 }}
+        />
+        </Marker>
+
+      
+      </MapView>
       <View style={styles.overlayContainer}>
         <Image source={require('../../../assets/images/INMOBINDER-03.png')} style={styles.logo} />
         <TextInput placeholder=" Buscar" style={styles.inputTexto} />
@@ -22,7 +88,9 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    flex: 1,
+    width: '100%',
+    height: '100%',
+
   },
   overlayContainer: {
     ...StyleSheet.absoluteFillObject,
