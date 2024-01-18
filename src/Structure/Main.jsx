4e591a5bb743f9af10a76d874a2d8b9cf84f6113
BenjahKPZz <ortizcuevasbenjamin@gmail.com>
2024-01-18 16:26:22 -0300

    Remove react-native-maps dependency

--- src/Structure/Main.jsx
+++ src/Structure/Main.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View} from 'react-native'
 
 const Main = () => {
     return (
-        <View style={styles.container}>
-            <Text>Wazaa</Text>
+        <View>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-    },
-  });
 
   export default Main
