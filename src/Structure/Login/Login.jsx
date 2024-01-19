024dcf2118c59edab9e2a432d66c470db60356c8
elbasurit4 <67765150+elbasurit4@users.noreply.github.com>
2024-01-19 11:46:13 -0300

    Google Signin Web

--- src/Structure/Login/Login.jsx
+++ src/Structure/Login/Login.jsx
@@ -4,18 +4,53 @@ import Constants from 'expo-constants';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
 
-
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut } from 'firebase/auth';
 
 export function Login() {
   const navigation = useNavigation();
 
-const goToStack = () => {
+  const goToStack = () => {
     navigation.navigate('DrawerS')
     navigation.reset({
-        index: 0,
-        routes: [{ name: 'DrawerS' }],
-      });
-};
+      index: 0,
+      routes: [{ name: 'DrawerS' }],
+    });
+  };
+
+  //Funcion para iniciar sesion con Google
+  async function signInWithGoogle() {
+    var config = {
+      apiKey: "AIzaSyDNu2t52GgXHdOupTmYH3zDWwK7Jt0mhJs",
+      authDomain: "agartha-marketing-agency.firebaseapp.com",
+      databaseURL: "https://agartha-marketing-agency-default-rtdb.firebaseio.com",
+      projectId: "agartha-marketing-agency",
+      storageBucket: "agartha-marketing-agency.appspot.com",
+      messagingSenderId: "122797123050",
+    };
+
+    const app = initializeApp(config);
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+ 
+    signInWithPopup(auth, provider).then(function(result) {
+      // Este da un token de acceso de Google. Puedes usarlo para acceder a la API de Google.
+      var token = result.credential.accessToken;
+      // La información del usuario que inició sesión.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Maneja los errores aquí.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // El correo electrónico de la cuenta del usuario que se utilizó.
+      var email = error.email;
+      // El tipo de firebase.auth.AuthCredential que se utilizó.
+      var credential = error.credential;
+      // ...
+    });
+  }
+
   return (
     <SafeAreaView style={styles.container}>
     
@@ -26,77 +61,81 @@ const goToStack = () => {
 
       {/* Contenido principal del inicio de sesión */}
       <View style={LoginStyles.containerLogin}>
-        <Text style={{ ...LoginStyles.textLogin  }}> Iniciar sesión </Text>
-        <Text style={{ ...LoginStyles.text1,  }}> ¿No tienes cuenta? <Text style={{ fontWeight:'bold' }} >Actívala</Text></Text>
-        <View style={{flexDirection:'row'}}>
-        <MaterialCommunityIcons name='at' size={25} style={{alignSelf:'center', paddingTop:25, paddingLeft:20}}/>
-        <TextInput onChange={(event) => setEmail(event.nativeEvent.text)} placeholder='Correo electrónico' style={{ ...LoginStyles.input, marginTop: '10%'}}/>
+        <Text style={{ ...LoginStyles.textLogin}}>Iniciar sesión</Text>
+        <Text style={{ ...LoginStyles.text1}}>¿No tienes cuenta?<Text style={{ fontWeight:'bold' }}> Actívala</Text></Text>
+
+        <View style={{flexDirection:'row', marginTop: 18}}>
+          <MaterialCommunityIcons name='at' size={25} style={{alignSelf:'center'}}/>
+          <TextInput onChange={(event) => setEmail(event.nativeEvent.text)} placeholder='Correo electrónico' style={{ ...LoginStyles.input}}/>
         </View>
+
         <View style={{flexDirection:'row'}}>
-        <MaterialCommunityIcons name='lock' size={25} style={{alignSelf:'center', paddingLeft:20}}/>
-        <TextInput onChange={(event) => setPassword(event.nativeEvent.text)} placeholder='Contraseña' style={LoginStyles.input} />
+          <MaterialCommunityIcons name='lock' size={25} style={{alignSelf:'center'}}/>
+          <TextInput onChange={(event) => setPassword(event.nativeEvent.text)} placeholder='Contraseña' style={LoginStyles.input}/>
         </View>
-        <Text style={{ ...LoginStyles.text2,  }}> Mantener la sesión iniciada</Text>
+
+        <Text style={{ ...LoginStyles.text2}}>Mantener la sesión iniciada</Text>
 
         {/* Botón de inicio de sesión */}
         <View>
-            {/* Inicio de sesión con Google */}
-            <TouchableOpacity style={LoginStyles.containerLoginGoogle}  onPress={goToStack}> 
-                        <Image source={require('./Data/google.png')} style={LoginStyles.imgGoogle}/>
-                        <Text style={{ ...LoginStyles.loginGoogle, }} > Ingresar con Google</Text>           
-            </TouchableOpacity>
+          {/* Inicio de sesión con Google */}
+          <TouchableOpacity style={LoginStyles.containerLoginGoogle}  onPress={signInWithGoogle}> 
+            <Image source={require('./Data/google.png')} style={LoginStyles.imgGoogle}/>
+            <Text style={{ ...LoginStyles.loginGoogle, }}>Ingresar con Google</Text>           
+          </TouchableOpacity>
 
           {/* Enlace para recuperar contraseña */}
-          <Text style={{ ...LoginStyles.RecoverPassword,  }} > ¿Has olvidado tu contraseña? </Text>
+          <Text style={{ ...LoginStyles.RecoverPassword}} > ¿Has olvidado tu contraseña? </Text>
         </View>
       </View>
+
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {     
-        alignItems: 'center',
-        flex: 1,
-        backgroundColor:'#fff'
-    },
-      title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-      },
-      separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-      },
+  container: {     
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor:'#fff'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
 
-      logoText: {
-        color: 'rgb(0,0,0)',
-        fontSize: 32,
-        fontWeight: "bold",
-      },
+  logoText: {
+    color: 'rgb(0,0,0)',
+    fontSize: 32,
+    fontWeight: "bold",
+  },
 
-      imgLogo: {
-        resizeMode: 'center',
-        width: 100,
-        height: 100,
-      },
+  imgLogo: {
+    resizeMode: 'center',
+    width: 100,
+    height: 100,
+  },
 
-      logoBack: {
-        color: 'rgb(0,0,0)',
-        marginRight: -10,
-      },
+  logoBack: {
+    color: 'rgb(0,0,0)',
+    marginRight: -10,
+  },
 
-      back:{
-        marginTop: Constants.statusBarHeight,
-        flexDirection: "row",
-        marginRight: "auto",
-        alignItems: "center",
-      },
+  back:{
+    marginTop: Constants.statusBarHeight,
+    flexDirection: "row",
+    marginRight: "auto",
+    alignItems: "center",
+  },
 
-    header:{
-      flexDirection: 'row',
-      alignItems: 'center',
+  header:{
+    flexDirection: 'row',
+    alignItems: 'center',
   },    
 })
 
@@ -104,104 +143,107 @@ export default Login;
 
 const LoginStyles = StyleSheet.create({
 
-    containerLogin:{    
-            width: "90%",
-            height: "70%",
-            borderRadius: 20,
-            alignItems: "center",
-            shadowOpacity: 1,
-            elevation: 4,
-            shadowRadius: 4,
-            shadowOffset: {
-                width: 0,
-                height: 4
-            },
-            shadowColor: "rgba(0, 0, 0, 1)",
-            backgroundColor: 'rgb(255, 255, 255)',
+  containerLogin:{    
+    width: "90%",
+    height: "70%",
+    borderRadius: 20,
+    alignItems: "center",
+    shadowOpacity: 1,
+    elevation: 4,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 0,
+      height: 4
     },
+    shadowColor: "rgba(0, 0, 0, 1)",
+    backgroundColor: 'rgb(255, 255, 255)',
+  },
 
-    textLogin:{
-        fontSize: 28,
-        marginTop: '3%',
-        fontWeight: 'bold',
-    },
+  textLogin:{
+    fontSize: 28,
+    marginTop: '6%',
+    fontWeight: 'bold',
+  },
 
-    text1:{
-        fontSize: 15,
-    },
+  text1:{
+    fontSize: 15,
+  },
 
-    text2:{
-        fontSize: 10,
-        width: "90%",
-        marginLeft: "5%",
-        marginTop: -10,
-    },
+  text2:{
+    fontSize: 10,
+    //width: "90%",
+    //marginLeft: "5%",
+    //marginTop: -10,
+  },
 
-    input:{
-        width: '90%',
-        height: 40,
-        borderWidth:2,
-        margin:10,
-        borderRadius:10
-    },
+  input:{
+    width: '85%',
+    height: 40,
+    borderWidth:2,
+    margin:10,
+    borderRadius:10,
+    paddingLeft: 10,
+  },
 
-    containerBtn:{
-        alignItems: "center",
-    },
+  containerBtn:{
+    alignItems: "center",
+  },
 
-    btnStyle:{
-        backgroundColor: 'rgb(9,27,43)',
-        borderRadius: 10,
-        width: 100,
-        height: 40,
-        marginTop: '10%',
-        shadowOpacity: 1,
-            elevation: 4,
-            shadowRadius: 4,
-            shadowOffset: {
-                width: 0,
-                height: 4
-            },
-            shadowColor: "rgba(0, 0, 0, 1)",
-    },
+  btnStyle:{
+    backgroundColor: 'rgb(9,27,43)',
+    borderRadius: 10,
+    width: 100,
+    height: 40,
+    marginTop: '10%',
+    shadowOpacity: 1,
+      elevation: 4,
+      shadowRadius: 4,
+      shadowOffset: {
+        width: 0,
+        height: 4
+      },
+      shadowColor: "rgba(0, 0, 0, 1)",
+  },
 
-    textBtn:{
-        color: 'rgb(255,255,255)',
-        fontSize: 14,
-        textAlign: "center",
-    },
+  textBtn:{
+    color: 'rgb(255,255,255)',
+    fontSize: 14,
+    textAlign: "center",
+  },
 
-    containerLoginGoogle:{
-        justifyContent: "center",
-        alignItems: 'center',
-        width: 220,
-        height: 40,
-        borderRadius: 40,
-        flexDirection: 'row',
-        shadowOpacity: 1,
-            elevation: 4,
-            shadowRadius: 4,
-            shadowOffset: {
-                width: 0,
-                height: 4
-            },
-            shadowColor: "rgba(0, 0, 0, 1)",
-            backgroundColor: 'rgb(255, 255, 255)',
-            marginTop: '3%',
-    },
+  containerLoginGoogle:{
+    justifyContent: "center",
+    alignItems: 'center',
+    marginTop: 10,
+    width: 220,
+    height: 40,
+    borderRadius: 40,
+    flexDirection: 'row',
+    shadowOpacity: 1,
+      elevation: 4,
+      shadowRadius: 4,
+      shadowOffset: {
+        width: 0,
+        height: 4
+      },
+      shadowColor: "rgba(0, 0, 0, 1)",
+      backgroundColor: 'rgb(255, 255, 255)',
+      marginTop: '3%',
+  },
 
-    loginGoogle:{
-        fontSize: 15,
-    },
+  loginGoogle:{
+    fontSize: 15,
+    paddingLeft: 10,
+  },
 
-    imgGoogle:{
-        width: 25,
-        height: 25,
-    },
+  imgGoogle:{
+    width: 25,
+    height: 25,
+  },
 
-    RecoverPassword:{
-        fontSize: 12,
-        textAlign: "center",
-        marginTop: '7%',
-    }
+  RecoverPassword:{
+    fontSize: 12,
+    textAlign: "center",
+    marginTop: '7%',
+  }
 });
