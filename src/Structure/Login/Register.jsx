8c563b8e067a0250cddfdd98a4f60dbc43d9ec9e
Tyrfing <jonathan.olivares2101@gmail.com>
2024-01-24 12:03:51 -0300

    Implementar boton de cerrar sesion

--- src/Structure/Login/Register.jsx
+++ src/Structure/Login/Register.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
-import { View, Text, StyleSheet, TouchableOpacity,Image, handlePress, TextInput , TouchableOpacity} from 'react-native'
+import { View, Text, StyleSheet,Image, handlePress, TextInput , TouchableOpacity} from 'react-native'
 
 import { auth } from './Data/firebase-config';
 import { getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
